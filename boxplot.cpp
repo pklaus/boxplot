275a1a23503668ec4ece5d8c7e2d4ce73fb311c3
Philipp Klaus <philipp.klaus@gmail.com>
2014-07-14 11:43:21 +0200

    Slimmed down, fast version of the boxplot c++ tool
    The original (slow) version using regex and a map<string,vector<double>>
    data structure was added in commit 4f36f44 and might still be useful later.

--- boxplot.cpp
+++ boxplot.cpp
@@ -1,14 +1,17 @@
+/*
+
+Slimmed down, fast version of the boxplot tool
+
+the original (slow) version using regex and a map<string,vector<double>>
+data structure was added in commit 4f36f44 and might still be useful later.
+
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
-#include <map>
 #include <algorithm>
 #include <vector>
-
-#include <cstdio>
-#include <cstdlib>
-
 #include <ctime>
 
 using namespace std;
@@ -17,13 +20,6 @@ double mean(vector<double> samples)
 {
     double sum = accumulate(samples.begin(), samples.end(), 0.0);
     return sum / samples.size();
-    //// standard deviation
-    //double m =  sum / v.size();
-    //double accum = 0.0;
-    //for_each (samples.begin(), samples.end(), [&](const double d) {
-    //    accum += (d - m) * (d - m);
-    //});
-    //stdev = sqrt(accum / (v.size()-1));
 }
 
 vector<double> quartiles(vector<double> samples)
@@ -35,7 +31,7 @@ vector<double> quartiles(vector<double> samples)
     sort(samples.begin(), samples.end());
     // First Quartile
     answer.push_back(samples[size/4]);
-    // Second Quartile
+    // Second Quartile = Median
     if (size % 2 == 0)
         answer.push_back((samples[size / 2 - 1] + samples[size / 2]) / 2);
     else
@@ -45,6 +41,19 @@ vector<double> quartiles(vector<double> samples)
     return answer;
 }
 
+void print_stats(string dateString, vector<double> data)
+{
+    sort(data.begin(), data.end());
+    vector<double> q = quartiles(data);
+    cout << dateString << " ";
+    cout << data.front() << " ";
+    cout << q[0] << " ";
+    cout << q[1] << " ";
+    cout << q[2] << " ";
+    cout << data.back() << " ";
+    cout << mean(data) << " ";
+    cout << data.size() << endl;
+}
 
 int main( int argc, char* argv[] )
 {
@@ -53,45 +62,40 @@ int main( int argc, char* argv[] )
     if( ! input_file ) return 2;
 
     string line_of_file;
-    smatch m;
-    regex e ("temperature: ([+-]?[0-9]+\\.[0-9]*)");   // matches words beginning by "sub"
 
     char dateoutput[100];
     struct tm timeDate;
-    string dateString;
+    string dateString = "";
+    double currValue;
 
-    map<string,vector<double>> data;
+    vector<double> data;
 
     while( !input_file.eof() ) {
         getline( input_file, line_of_file );
         if( input_file ) {
-            if (regex_search (line_of_file,m,e)) {
+            string search = " temperature: ";
+            std::size_t found = line_of_file.find(search);
+            if (found != string::npos) {
+                currValue = stod(line_of_file.substr(found + search.size()));
                 strptime(line_of_file.substr(0,19).c_str(),"%Y-%m-%d_%H:%M:%S", &timeDate);
-                if (!strftime(dateoutput, sizeof(dateoutput), "%F", &timeDate)) {
+                if (!strftime(dateoutput, sizeof(dateoutput), "%F", &timeDate))
                     continue;
+                if (data.size() == 0)
+                {
+                    cout << "# date whisker_low box_low median box_high whisker_high mean nsamples" << endl;
+                    dateString = string(dateoutput);
                 }
-                dateString = string(dateoutput);
-                if (!data.count(dateString)>0)
-                    data[dateString] = vector<double>({stod(m[1])});
+                if (dateString == string(dateoutput))
+                    data.push_back(currValue);
                 else
-                    data[dateString].push_back(stod(m[1]));
+                {
+                    print_stats(dateString, data);
+                    dateString = string(dateoutput);
+                    data = vector<double>({currValue});
+                }
             }
         }
         else break;
-    }
-
-    for(auto iter=data.begin(); iter!=data.end(); ++iter) {
-        sort(iter->second.begin(), iter->second.end());
-        vector<double> q = quartiles(iter->second);
-        cout << iter->first << " ";
-        cout << iter->second.front() << " ";
-        cout << q[0] << " ";
-        cout << q[1] << " ";
-        //cout << median(iter->second) << " ";
-        cout << q[2] << " ";
-        cout << iter->second.back() << " ";
-        cout << mean(iter->second) << " ";
-        cout << iter->second.size() << endl;
     }
 
   return 0;
